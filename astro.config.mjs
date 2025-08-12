99c02c7680b3c0b9ce1e28744d932a246b83b212
Efterklang <gaojiaxing2004@qq.com>
2025-08-12 23:43:43 +0800

    feat: update configuration and add new guides for Vluv's Wiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,37 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import catppuccin from "@catppuccin/starlight";
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-      plugins: [catppuccin()],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Vluv\'s Wiki',
+			plugins: [catppuccin({
+				dark: { flavor: "mocha", accent: "lavender" },
+				light: { flavor: "latte", accent: "lavender" },
+			})],
+			expressiveCode: {
+				themes: ['catppuccin-mocha', 'catppuccin-latte'],
+				plugins: [pluginLineNumbers(), pluginCollapsibleSections()],
+				styleOverrides: {
+					uiFontFamily: "Maple Mono CN, Maple Mono NF CN, monospace"
+				}
+			},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Efterklang' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'About', slug: 'guides/example' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'ARM',
+					autogenerate: { directory: 'ARM-Processor' },
 				},
 			],
 		}),
