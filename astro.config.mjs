81c4b0cc6800e235c871068623c747eb17f0c80f
Efterklang <gaojiaxing2004@qq.com>
2025-08-12 19:42:17 +0800

    build(theme): catppuccin theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import catppuccin from "@catppuccin/starlight";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+      plugins: [catppuccin()],
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
